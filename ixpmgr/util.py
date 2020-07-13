a8ecd376c185e46caf75086c26b3ea8f57e2666e
Matt Griswold <grizz@20c.com>
2020-07-13 10:18:53 -0500

    Rmpy2 (#1)
    * fix long_description readme file error
    
    * 2to3
    
    * pyupgrade
    
    * black format
    
    Co-authored-by: Stefan Pratter <stefan@20c.com>

--- ixpmgr/util.py
+++ ixpmgr/util.py
@@ -1,5 +1,4 @@
-
-import ConfigParser
+import configparser
 from collections import namedtuple
 import os
 import re
@@ -16,11 +15,11 @@ def load_config():
     load the IXP-Manager ini config into a dict and return it
     """
     home = settings.HOME
-    cfg_file = os.path.join(home, 'application', 'configs', 'application.ini')
+    cfg_file = os.path.join(home, "application", "configs", "application.ini")
     if not os.path.exists(cfg_file):
-        raise ValueError("Config file %s not found" % (cfg_file,))
+        raise ValueError(f"Config file {cfg_file} not found")
 
-    parser = ConfigParser.ConfigParser()
+    parser = configparser.ConfigParser()
     parser.read(cfg_file)
 
     rv = {}
@@ -32,15 +31,19 @@ def load_config():
             rv[section][k] = v
     return rv
 
-Interface = namedtuple('Interface', ['phy', 'vir'])
+
+Interface = namedtuple("Interface", ["phy", "vir"])
+
 
 def get_interface(vir_intf_id):
     vir_intf = VirtualInterface.objects.get(pk=vir_intf_id)
     phy_intf = PhysicalInterface.objects.get(pk=each.id)
 
+
 def parse_macaddr(addr):
-    addr = re.sub('[\.\s:-]+', '', addr)
-    return "{0:012x}".format(int(addr, 16))
+    addr = re.sub(r"[\.\s:-]+", "", addr)
+    return "{:012x}".format(int(addr, 16))
+
 
 def get_macaddr(virt_intf):
     qs = MacAddress.objects.filter(virtual_interface__id=virt_intf.id)
@@ -48,12 +51,14 @@ def get_macaddr(virt_intf):
     if cnt == 1:
         return qs[0]
     elif not cnt:
-        raise ValueError('no mac addresses defined for interface')
+        raise ValueError("no mac addresses defined for interface")
     else:
-        raise ValueError('multiple mac addresses already defined for interface')
+        raise ValueError("multiple mac addresses already defined for interface")
+
 
 def format_macaddr(addr):
-	return ':'.join(map(''.join, zip(*[iter(addr)] * 2)))
+    return ":".join(map("".join, list(zip(*[iter(addr)] * 2))))
+
 
 def dns_intf_name(intf):
     regex = settings.RDNS_INTF_REGEX
@@ -61,4 +66,3 @@ def dns_intf_name(intf):
     for (pattern, repl) in regex:
         intf = re.sub(pattern, repl, intf)
     return intf
-
