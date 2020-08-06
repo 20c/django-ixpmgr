aaec7f15c13eed49cb751e34a78fb3512eb31f2c
Roy Crihfield <roy@manteia.ltd>
2020-08-06 01:14:09 -0500

    json default

--- ixpmgr/ixapi/settings.py
+++ ixpmgr/ixapi/settings.py
@@ -30,6 +30,12 @@ ALLOWED_HOSTS = ["*"]
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
 }
 
 # Application definition
