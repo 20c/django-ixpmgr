7f932a9cc6025d3d7b9c2ef988c58668d40426d6
Roy Crihfield <roy@manteia.ltd>
2020-08-07 00:47:50 -0500

    static api schema

--- ixpmgr/ixapi/settings.py
+++ ixpmgr/ixapi/settings.py
@@ -140,3 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+# where collectstatic will collect static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = [
+    os.path.join(STATIC_ROOT, "static"),
+]
--- ixpmgr/ixapi/urls.py
+++ ixpmgr/ixapi/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from rest_framework import routers
 
 from . import views
@@ -34,3 +38,8 @@ urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
