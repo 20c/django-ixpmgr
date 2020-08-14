91cd2e9e64dcf235bded400cc887ae7eecf5736f
Stefan Pratter <stefan@20c.com>
2020-08-14 11:31:59 +0000

    network-service-config finalize (for now)

--- ixpmgr/config/models.py
+++ ixpmgr/config/models.py
@@ -12,6 +12,9 @@ class Connection(ixpmgr_models.Virtualinterface):
     A Connection is a functional group of physical connections collected together into a LAG (aka trunk).
 
     A connection with only one port can be also configured as standalone which means no LAG configuration on the switch.
+
+    Currently only implemented in the most basic form so we can reference
+    it in the ExchangeLanNetworkServiceConfig model
     """
 
     proxies = ProxyManager()
@@ -50,29 +53,57 @@ class ExchangeLanNetworkServiceConfig(ixpmgr_models.Vlaninterface):
     class Meta:
         proxy = True
 
-    status = NullField()
-    managing_account = NullField()
-    consuming_account = NullField()
-    billing_account = NullField()
-    external_ref = NullField()
-    purchase_order = NullField()
     contract_ref = NullField()
-    vlan_config = NullField()
-    capacity = NullField()
+    purchase_order = NullField()
 
     @property
     def _customer(self):
+        """
+        helper function to get the related IXPMGR customer object
+        """
         return self.virtualinterfaceid.custid
 
     @property
     def _connection(self):
+        """
+        helper function to get the related IXAPI connection object
+        """
         return Connection.objects.get(id=self.virtualinterfaceid.id)
 
     @property
+    def _network_service(self):
+        """
+        helper function to get the related IXAPI networkservice object
+        """
+        return service_models.ExchangeLanNetworkService.objects.get(id=self.vlanid.infrastructureid.id)
+
+    @property
     def _physical(self):
+        """
+        helper function to get the related IXPMGR physicalinterface object
+        """
         return ixpmgr_models.Physicalinterface.objects.get(
             virtualinterfaceid=self.virtualinterfaceid
         )
+
+    @property
+    def _switch(self):
+        """
+        helper function to get the related IXPMGR switch object
+        """
+        return self._physical.switchportid.switchid
+
+
+    @property
+    def capacity(self):
+
+        # the only specification of speed / capacity
+        # we found in the ixp manager schema is
+        # directly on the physical interface, so this what
+        # we pass through for now - may need to take a close
+        # look at this
+
+        return self._physical.speed
 
     @property
     def state(self):
@@ -80,27 +111,38 @@ class ExchangeLanNetworkServiceConfig(ixpmgr_models.Vlaninterface):
         ns_state = self._network_service.state
         phy_state = self._physical.status
 
+        # if network-service state is anything but
+        # PRODUCTION simply pass through that state
+
         if ns_state != State.PRODUCTION:
             return ns_state
+
+        # if physical interface state is connected
+        # return PRODUCTION state
 
         if phy_state == 1:
             # connected
             return State.PRODUCTION
 
-
-        #TODO: better mapping?
+        # otherwise return ALLOCATED state for now
+        #TODO: better mapping
         """
         1 = CONNECTED
         2 = DISABLED
         3 = NOT CONNECTED
-        4 = AWAITING
+        4 = AWAITING X-CONNECT
         5 = QUARANTINE
         """
 
         return State.ALLOCATED
 
     @property
+    def status(self):
+        return []
+
+    @property
     def managing_account(self):
+        # should this be the ix or the network?
         return self._customer.id
 
     @property
@@ -109,19 +151,22 @@ class ExchangeLanNetworkServiceConfig(ixpmgr_models.Vlaninterface):
 
     @property
     def billing_account(self):
+        # doesn't look like ixp manager separates
+        # billing accounts from customer accounts
         return self._customer.id
 
     @property
     def listed(self):
+        # TODO: how is this determined?
         return True
 
     @property
-    def type(self):
-        return ""
+    def external_ref(self):
+        return f"vlaninterface:{self._customer.autsys}:{self.pk}"
 
     @property
-    def _network_service(self):
-        return service_models.ExchangeLanNetworkService.objects.get(id=self.vlanid.infrastructureid.id)
+    def type(self):
+        return "exchange_lan"
 
     @property
     def network_service(self):
@@ -140,13 +185,16 @@ class ExchangeLanNetworkServiceConfig(ixpmgr_models.Vlaninterface):
         return []
 
     @property
+    def vlan_config(self):
+        return Dot1QVLanConfig.objects.get(id=self.vlanid.id)
+
+    @property
     def asns(self):
-        return []
+        return [self._customer.autsys]
 
     @property
     def ip_addresses(self):
         # todo - check vlaninterface.ipvXenabled?
-        # todo - exclude vlan.private>0 ?
 
         ips = []
 
@@ -181,8 +229,23 @@ class QinQVLanConfig(models.Model):
     pass
 
 
-class Dot1QVLanConfig(models.Model):
-    pass
+class Dot1QVLanConfig(ixpmgr_models.Vlan):
+    class Meta:
+        proxy = True
+
+    proxies = ProxyManager()
+    Source = ixpmgr_models.Vlan
+
+    vlan = ProxyField(Source.id)
+
+
+    vlan_ethertype = NullField()
+
+    @property
+    def vlan_type(self):
+        return "dot1q"
+
+
 
 
 class NetworkServiceConfig(models.Model):
