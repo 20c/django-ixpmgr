cedada73f3a1964150b2355afde51391b924e8f6
Roy Crihfield <roy@manteia.ltd>
2020-08-24 23:04:19 -0500

    rename django app

--- src/django_ixpmgr/v57/__init__.py
+++ src/django_ixpmgr/v57/__init__.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class IxpManagerConfig(AppConfig):
     name = "django_ixpmgr.v57"
     # Must be unique, will conflict if two versions are used
-    label = "ixpmgr"
+    label = "django_ixpmgr_ixapi"
