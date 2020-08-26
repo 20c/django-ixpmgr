c906c3162bc72280933ac101e0dbd1854b039465
egfrank <elliot.g.frank@gmail.com>
2020-08-26 15:15:23 -0500

    Add catalog and service model tests

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -1,10 +1,19 @@
 import pytest
 from pprint import pprint
+from itertools import chain
+
+from django.conf import settings
+
 import crm.models
 import catalog.models
 import service.models
 import ipam.models
 import config.models
+from ixapi_schema.v2.constants import (
+    config as config_const, ipam as ipam_const
+)
+
+
 
 # Helpers 
 def assertEqual(a,b, *args):
@@ -13,7 +22,14 @@ def assertEqual(a,b, *args):
         for arg in args:
             assert a == arg
 
-# CRM tests
+def get_ip_addresses(instance):
+    ipv4 = []
+    for vlan in instance.vlan_set.all():
+        addrs = ipam.models.IpAddress.objects.filter(vlanid=vlan.id)
+        ipv4.extend(addrs)
+    return ipv4
+
+# ------- CRM tests ----------
 @pytest.mark.django_db
 def test_reg_address(ixpmgr_data, ixpmgr_models):
     _id = 1
@@ -71,7 +87,106 @@ def test_account(ixpmgr_data, ixpmgr_models):
     assertEqual(account.peeringdb_oauth, 1)
 
 
-# Catalog tests
+# ------- Catalog tests ----------
+@pytest.mark.django_db
+def test_facility(ixpmgr_data, ixpmgr_models):
+    _id = 1
+    facility = catalog.models.Facility.objects.get(id=_id)
+    location = ixpmgr_models.Location.objects.get(id=_id)
+    expected = ixpmgr_data["ixpmgr.location"][_id]
+
+    assertEqual(facility.metro_area, location.city, expected["city"])
+    assertEqual(facility.address_country, location.country, expected["country"])
+    assertEqual(facility.address_locality, location.city, expected["city"])
+    assertEqual(facility.address_region, None)
+    assertEqual(facility.postal_code, None)
+    assertEqual(facility.street_address, location.address, expected["address"])
+    assertEqual(facility.peeringdb_facility_id, location.pdb_facility_id, expected["pdb_facility_id"])
+    assertEqual(facility.cluster, None)
+    assertEqual(facility.operator_name, "Not specified")
+
+@pytest.mark.django_db
+def test_device(ixpmgr_data, ixpmgr_models):
+    _id = 1
+    device = catalog.models.Device.objects.get(id=_id)
+    switch = ixpmgr_models.Switch.objects.get(id=_id)
+    expected = ixpmgr_data["ixpmgr.switch"][_id]
+
+    assertEqual(device.pop, None)
+    assertEqual(device.capabilities, None)
+    assertEqual(device.facility, catalog.models.Facility.objects.get(id=device.infrastructure.id))
 
 
+# ------- Service tests ----------
+@pytest.mark.django_db
+def test_allow_member_joining_rule(ixpmgr_data, ixpmgr_models):
+    _id = 1
+    amjr = service.models.AllowMemberJoiningRule.objects.get(id=_id)
+    cust = ixpmgr_models.Cust.objects.get(id=_id)
+    expected = ixpmgr_data["ixpmgr.cust"][_id]
 
+    assertEqual(amjr.managing_account, None)
+    assertEqual(amjr.consuming_account, None)
+    assertEqual(amjr.network_service, None)
+    assertEqual(amjr.test_cust_name, cust.name, expected["name"])
+    assertEqual(amjr.capacity_min, None)
+    assertEqual(amjr.capacity_max, None)
+
+@pytest.mark.django_db
+def test_exchange_lan_network_service(ixpmgr_data, ixpmgr_models):
+    _id = 1
+    elns = service.models.ExchangeLanNetworkService.objects.get(id=_id)
+    infra = ixpmgr_models.Infrastructure.objects.get(id=_id)
+    expected = ixpmgr_data["ixpmgr.infrastructure"][_id]
+
+    assertEqual(elns.external_ref, None)
+    assertEqual(elns.metro_area, "IDK")
+    assertEqual(elns.product_offering, None)
+    assertEqual(elns.all_nsc_required_contact_roles, None)
+    assertEqual(elns.peeringdb_ixid, infra.peeringdb_ix_id, expected["peeringdb_ix_id"])
+    assertEqual(elns.ixfdb_ixid, infra.ixf_ix_id, expected["ixf_ix_id"])
+    assertEqual(elns.status, None)
+
+    customer = ixpmgr_models.CustomerToIxp.objects.filter(ixp=elns.ixp).first().customer
+    state = customer.status
+    qsets = (
+            service.models.RouteServerNetworkFeature.objects.filter(vlan=vlan).all()
+            for vlan in elns.vlan_set.all()
+            )
+    network_features = list(chain(*qsets))
+
+    assertEqual(elns.network_features, network_features)
+    assertEqual(elns.managing_account, customer.id)
+    assertEqual(elns.consuming_account, customer.id)
+    assertEqual(elns.type, "exchange_lan")
+    #FIXME
+    # import not working
+    # assertEqual(elns.state, None)
+    ipv4 = get_ip_addresses(elns)
+    assertEqual(elns.ip_addresses, ipv4)
+    assertEqual(elns.isprimary, True)
+
+@pytest.mark.django_db
+def test_route_server_network_feature(ixpmgr_data, ixpmgr_models):
+    _id = 1
+    rsnf = service.models.RouteServerNetworkFeature.objects.get(id=_id)
+    infra = ixpmgr_models.Infrastructure.objects.get(id=_id)
+    expected = ixpmgr_data["ixpmgr.infrastructure"][_id]
+
+    assertEqual(rsnf.fqdn, "example.com")
+    assertEqual(rsnf.required, False)
+    assertEqual(rsnf.all_nfc_required_contact_roles, None)
+    assertEqual(rsnf.ixp_specific_flags, [])
+
+    assertEqual(rsnf.network_service, service.models.ExchangeLanNetworkService.objects.get(vlan=rsnf.vlan))
+    assertEqual(rsnf.looking_glass_url, "https://lg.moon-ix.net/rs1")
+
+    assertEqual(rsnf.address_families, [ipam_const.AddressFamilies.AF_INET])
+    # assertEqual(rsnf.session_mode, config_const.RouteServerSessionMode.MODE_PUBLIC)
+    assertEqual(rsnf.available_bgp_session_types, None)
+
+    #FIXME property isn't working on RouteServerNetworkFeature
+    # ipv4 = get_ip_addresses(rsnf)
+    # assertEqual(rsnf.ip_addresses, ipv4)
+
+# ------- IPAM tests ----------
