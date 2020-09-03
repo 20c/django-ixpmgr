b8347cf8d9c038be44fcfc4c95ae3ea5aa88f8cd
Stefan Pratter <stefan@20c.com>
2020-09-03 16:07:11 +0000

    remove double import

--- src/django_ixpmgr/v57/__init__.py
+++ src/django_ixpmgr/v57/__init__.py
@@ -1,7 +1,5 @@
 from django.apps import AppConfig
 
-from django.apps import AppConfig
-
 class IxpManagerAppConfig(AppConfig):
     name = "django_ixpmgr.v57"
     # Must be unique, will conflict if two versions are used
