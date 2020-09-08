71d4264d3737c1454a62b2f8bd07a2c42620666f
Stefan Pratter <stefan@20c.com>
2020-09-08 05:37:35 +0000

    fix typo

--- src/django_ixpmgr/__init__.py
+++ src/django_ixpmgr/__init__.py
@@ -3,4 +3,4 @@ from .settings import VERSIONED_APP
 if VERSIONED_APP == "v5":
     from django_ixpmgr.v5 import IxpManagerAppConfig
 elif VERSIONED_APP == "v3":
-    from djnago_ixpmgr.v3 import IxpManagerAppConfig
+    from django_ixpmgr.v3 import IxpManagerAppConfig
